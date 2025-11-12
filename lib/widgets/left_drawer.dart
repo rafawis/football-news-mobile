950591ab4ef92599b78a97f977bc478f42a73ad2
rafawis <rafa.wisnutomo@gmail.com>
2025-11-12 14:33:58 +0700

    tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart';
 import 'package:football_news/widgets/news_card.dart';
+import 'package:football_news/screens/news_entry_list.dart';
+
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -69,10 +71,21 @@ class LeftDrawer extends StatelessWidget {
               Navigator.pushReplacement(
               context,
               MaterialPageRoute(
-              builder: (context) => MyHomePage(),
+                  builder: (context) => const NewsEntryListPage()
               ));
               },
-          )
+          ),
+          ListTile(
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('News List'),
+            onTap: () {
+            // Route to news list page
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+              );
+            },
+          ),
         ],
       ),
     );
